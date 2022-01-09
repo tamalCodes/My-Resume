495adc51afce01ca3f979bbb598c30e924bbedd7
Tamal Das <gyansujan69@gmail.com>
2022-01-09 11:23:25 +0530

    Updated Projects.

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -12,13 +12,19 @@ const Project = () => {
 
                 <h4 className="pro_parent_1_header1">My Projects <i class="fas fa-code i1"></i></h4>
 
-                <a href="https://iamtamal.github.io/Covid-19/"><img src="https://i.ibb.co/XxFJ7W2/pr1.png" className="prs" alt="" /></a>
+                <a href="https://iamtamal.github.io/Co-Winn/"><img src="https://i.ibb.co/XxFJ7W2/pr1.png" className="prs" alt="" /></a>
 
                 <a href="https://iamtamal.github.io/avatar-generator/"><img src="https://i.ibb.co/7pWxWpS/pr2.png" className="prs" alt="" /></a>
 
                 <a href="https://clone-46584.web.app/"><img src="https://i.ibb.co/gRQkc9s/pr4.png" className="prs" alt="" /></a>
 
                 <a href="https://iamtamal.github.io/Milan/"><img src="https://i.ibb.co/3My9Z0X/pr5.png" className="prs" alt="" /></a>
+
+                <a href="https://pholbum.netlify.app/"><img src="https://i.ibb.co/B3SHgHr/PhoLbum.png" className="prs" alt="" /></a>
+
+                <a href="https://class-book.netlify.app/"><img src="https://i.ibb.co/wSpkKd8/class-book.png" className="prs" alt="" /></a>
+
+                <a href="https://memoirs-tamal.netlify.app/"><img src="https://i.ibb.co/Psy7Jfz/memoirs.png" className="prs" alt="" /></a>
             </div>
 
             <div className="container-fluid" id='pro_parent_2'>
